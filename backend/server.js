1a9574226c861da62bed42eb7be1d2490e838406
Andrea <a01286027@tec.mx>
2025-10-26 00:54:31 +0000

    send to me

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, '../frontend')));
 
 // Root route explicitly sends index.html
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/index.html'));
+  res.sendFile(path.join(__dirname, '../frontend/login.html'));
 });
 
 
